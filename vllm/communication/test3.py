6006091c5185a771b7c49a8c9e3b383f3f3b5267
hiGiraffe <chenjulian.buaa@gmail.com>
2024-05-13 12:05:45 +0800

    2

--- vllm/communication/test3.py
+++ vllm/communication/test3.py
@@ -8,6 +8,9 @@ def communicate():
     cuda1 = torch.device('cuda', 0)
     cuda2 = torch.device('cuda', 1)
 
+    torch.cuda.synchronize(cuda1)
+    torch.cuda.synchronize(cuda2)
+
     # warm up
     for _ in range(100000):
         a = torch.randn((2, 3), device=cuda2)
@@ -17,23 +20,40 @@ def communicate():
     torch.cuda.synchronize(cuda2)
 
     # 第一次测试从CPU到GPU数据加载时间
-    a = torch.randn((2, 3), device=cpu)
+    a = torch.randn((200, 300), device=cpu)
     a.to(cuda1)
     torch.cuda.synchronize(cuda1)
 
+
     # 测试从GPU到GPU数据加载时间
-    a = torch.randn((2, 3), device=cuda2)
+    a = torch.randn((200, 300), device=cuda2)
     a.to(cuda1)
     torch.cuda.synchronize(cuda1)
     torch.cuda.synchronize(cuda2)
 
     # 测试从GPU到CPU数据加载时间
-    a = torch.randn((2, 3), device=cuda1)
+    a = torch.randn((200, 300), device=cuda1)
     a.to(cpu)
     torch.cuda.synchronize(cuda1)
 
     # 第二次测试从CPU到GPU数据加载时间
-    a = torch.randn((2, 3), device=cpu)
+    a = torch.randn((200, 300), device=cpu)
+    a.to(cuda1)
+    torch.cuda.synchronize(cuda1)
+
+    # 第二次测试从GPU到GPU数据加载时间
+    a = torch.randn((200, 300), device=cuda2)
+    a.to(cuda1)
+    torch.cuda.synchronize(cuda1)
+    torch.cuda.synchronize(cuda2)
+
+    # 第二次测试从GPU到CPU数据加载时间
+    a = torch.randn((200, 300), device=cuda1)
+    a.to(cpu)
+    torch.cuda.synchronize(cuda1)
+
+    # 第三次测试从CPU到GPU数据加载时间
+    a = torch.randn((200, 300), device=cpu)
     a.to(cuda1)
     torch.cuda.synchronize(cuda1)
 
