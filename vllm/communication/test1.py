5a6dbbf36baebc4f6130ab28810c5b4323701ed0
hiGiraffe <chenjulian.buaa@gmail.com>
2024-05-13 10:37:29 +0800

    communication-1

---
+++ vllm/communication/test1.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+import torch
+import time
+
+cuda1 = torch.device('cuda', 0)
+cpu = torch.device('cpu', 0)
+# 测试从CPU到GPU数据加载时间
+s = time.time()
+for _ in range(100000):
+    a = torch.randn((2, 3), device=cpu)
+    a.to(cuda1)
+print('cpu --> gpu cost time is', time.time() - s)
+
+# 测试从GPU到GPU数据加载时间
+cuda1 = torch.device('cuda', 0)
+cuda2 = torch.device('cuda', 1)
+s = time.time()
+for _ in range(100000):
+    a = torch.randn((2, 3), device=cuda2)
+    a.to(cuda1)
+print('gpu --> gpu cost time is', time.time() - s)
