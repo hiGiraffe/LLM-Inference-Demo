d63ca1e2adbe1bf5c5b60610cc32df13dd316964
hiGiraffe <chenjulian.buaa@gmail.com>
2024-05-19 22:59:51 +0800

    offline

--- vllm/offline_inference.py
+++ vllm/offline_inference.py
@@ -1,20 +1,21 @@
 from vllm import LLM, SamplingParams
 from line_profiler import LineProfiler
+# Sample prompts.
+prompts = [
+    "Hello, my name is",
+    "The president of the United States is",
+    "The capital of France is",
+    "The future of AI is",
+]
+# Create a sampling params object.
+sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+
+# Create an LLM.
+llm = LLM(model="meta-llama/Llama-2-7b-chat-hf",enforce_eager=True)
 
 @profile
-def offline():
-    # Sample prompts.
-    prompts = [
-        "Hello, my name is",
-        "The president of the United States is",
-        "The capital of France is",
-        "The future of AI is",
-    ]
-    # Create a sampling params object.
-    sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+def generate():
 
-    # Create an LLM.
-    llm = LLM(model="meta-llama/Llama-2-7b-chat-hf",enforce_eager=True)
     # Generate texts from the prompts. The output is a list of RequestOutput objects
     # that contain the prompt, generated text, and other information.
     outputs = llm.generate(prompts, sampling_params)
@@ -26,4 +27,4 @@ def offline():
         print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
 
 if __name__ == '__main__':
-    offline()
+    generate()
