4d048e39ebf7253f699262dd24f82c38b6988c5c
hiGiraffe <chenjulian.buaa@gmail.com>
2024-06-20 21:18:25 +0800

    update

--- vllm/offline_inference.py
+++ vllm/offline_inference.py
@@ -1,5 +1,6 @@
 from vllm import LLM, SamplingParams
 from line_profiler import LineProfiler
+
 # Sample prompts.
 prompts = [
     "Hello, my name is",
@@ -15,24 +16,39 @@ prompts = [
     "The capital of France is",
     "The future of AI is",
 ]
-# Create a sampling params object.
-sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
-
 # Create an LLM.
-llm = LLM(model="meta-llama/Llama-2-7b-chat-hf",enforce_eager=True)
+llm = LLM(model="meta-llama/Llama-2-7b-chat-hf", enforce_eager=True)
+
 
 @profile
-def generate():
-
+def generate100(num=100):
     # Generate texts from the prompts. The output is a list of RequestOutput objects
     # that contain the prompt, generated text, and other information.
+    # Create a sampling params object.
+    sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=num, min_tokens=num)
     outputs = llm.generate(prompts, sampling_params)
-    #outputs = llm.generate(prompts)
+    # outputs = llm.generate(prompts)
     # Print the outputs.
     for output in outputs:
         prompt = output.prompt
         generated_text = output.outputs[0].text
         print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
 
+@profile
+def generate200(num=200):
+    # Generate texts from the prompts. The output is a list of RequestOutput objects
+    # that contain the prompt, generated text, and other information.
+    # Create a sampling params object.
+    sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=num, min_tokens=num)
+    outputs = llm.generate(prompts, sampling_params)
+    # outputs = llm.generate(prompts)
+    # Print the outputs.
+    for output in outputs:
+        prompt = output.prompt
+        generated_text = output.outputs[0].text
+        print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+
+
 if __name__ == '__main__':
-    generate()
+    generate100()
+    generate200
